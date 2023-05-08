a0b7e37a1f814fe4d3d8b6d183484eb3024efc09
dietervansteenwegen <github@vansteenwegen.org>
2023-05-08 11:18:45 +0200

    Preparations for v1.0.0 release

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@
 from setuptools import find_packages
 from setuptools import setup
 
-setup(name='p_credentials', version='0.1.0', packages=find_packages())
+setup(name='p_credentials', version='1.0.0', packages=find_packages())
